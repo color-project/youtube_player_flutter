768309e7a2ac88cbb38ad46f50087093db8d2f3d
eden <yeomdonguk>
2022-01-06 15:57:57 +0900

    first init

--- packages/youtube_player_iframe/lib/src/helpers/player_fragments.dart
+++ packages/youtube_player_iframe/lib/src/helpers/player_fragments.dart
@@ -33,10 +33,10 @@ String youtubeIFrameTag(YoutubePlayerController controller) {
     'embed/${controller.initialVideoId}',
     params,
   );
-  return '<iframe id="player" type="text/html"'
+  return '<div style="pointer-events: none"><iframe id="player" type="text/html"'
       ' style="position:absolute; top:0px; left:0px; bottom:0px; right:10px;'
       ' width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"'
-      ' src="$sourceUri" frameborder="0" allowfullscreen></iframe>';
+      ' src="$sourceUri" frameborder="0" allowfullscreen></iframe></div>';
 }
 
 ///
